403c407791d810ecda6b20c7d0db2d89c44f47f8
javasusanto <herisusanto.id@gmail.com>
2018-10-30 09:58:24 +0700

    Palindrome Swapter

--- palindromeSwapper.js
+++ palindromeSwapper.js
@@ -8,13 +8,13 @@
 function palindromeSwapper(str) {
   let newStr = '';
   for (var i = 0; i < str.length; i++) {
-    for (var j = 0; j < str.length; i++) {
+    for (var j = 0; j < str.length; j++) {
       if (i === j) {
-        newStr += str[j++];
+        newStr += str[j + 1];
         newStr += str[j];
         j++;
       } else {
-        newStr += str[j];
+        newStr += str[j]
       }
     }
     if (isPalindrome(newStr)) return true;
@@ -29,8 +29,8 @@ function palindromeSwapper(str) {
  * @returns {boolean} true bila kata adalah palindrom
  */
 function isPalindrome(str) {
-  if (str.split('').reverse().join() === str) return console.log(true);
-  return console.log(false);
+  if (str.split('').reverse().join('') === str) return true
+  return false
 }
 
 console.log(palindromeSwapper('arcecar')); // TRUE
