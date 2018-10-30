10bfd9dd3147614c44df6ecdf09ac8221a52a864
Zhang <zhang@android-42520aa2424d51bc.localdomain>
2018-10-30 10:44:04 +0700

    debug palindrom

--- palindromeSwapper.js
+++ palindromeSwapper.js
@@ -1,4 +1,4 @@
-/**
+/*
  * @function palindromeSwapper
  * menukar 2 huruf berurutan dari n = 0 sampai n-1
  * contoh: makan => amkan, mkaan, maakn, makna
@@ -6,31 +6,45 @@
  * @returns {boolean} true bila kata yang di-swap adalah palindrom
  */
 function palindromeSwapper(str) {
-  let newStr = '';
-  for (var i = 0; i < str.length; i++) {
-    for (var j = 0; j < str.length; i++) {
-      if (i === j) {
-        newStr += str[j++];
-        newStr += str[j];
-        j++;
-      } else {
-        newStr += str[j];
+  if (isPalindrome(str)){
+    return true 
+  }else{
+
+    for (var i = 0; i < str.length; i++) {
+      let newStr = '';
+      debugger
+      
+      for (var j = 0; j < str.length; j++) {
+        debugger
+        if (i === j && j !== str.length) {
+          newStr += str[j+1];
+          newStr += str[j];
+          j++;
+        } else {
+          newStr += str[j];
+        }
       }
+      if (isPalindrome(newStr)){
+        return true
+      }else{
+        return false;
+      };
     }
-    if (isPalindrome(newStr)) return true;
   }
-  return false;
 }
 
-/**
+/*
  * @function isPalindrome
  * Cek apabila kata merupakan palindromeSwapper
  * @param {string} str - input kata yang dicek bila palindrom
  * @returns {boolean} true bila kata adalah palindrom
  */
 function isPalindrome(str) {
-  if (str.split('').reverse().join() === str) return console.log(true);
-  return console.log(false);
+  if (str.split('').reverse().join('') === str) {
+    return true
+    debugger
+  };
+  return false;
 }
 
 console.log(palindromeSwapper('arcecar')); // TRUE
