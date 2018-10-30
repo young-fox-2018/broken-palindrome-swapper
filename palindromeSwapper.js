874f40de67d53be003927b6aa673180812acc998
fauzia <fauziaazia171097@gmail.com>
2018-10-30 10:43:56 +0700

    palindrome_swapper

--- palindromeSwapper.js
+++ palindromeSwapper.js
@@ -6,20 +6,31 @@
  * @returns {boolean} true bila kata yang di-swap adalah palindrom
  */
 function palindromeSwapper(str) {
-  let newStr = '';
+  if(isPalindrome(str)) return true;
+  
   for (var i = 0; i < str.length; i++) {
-    for (var j = 0; j < str.length; i++) {
-      if (i === j) {
-        newStr += str[j++];
+    let newStr = '';
+    for (var j = 0; j < str.length; j++) {
+      if (i === j && j !== str.length) {
+
+        newStr += str[j+1];
         newStr += str[j];
         j++;
       } else {
         newStr += str[j];
       }
+      // debugger
     }
-    if (isPalindrome(newStr)) return true;
+
+    if (isPalindrome(newStr)){
+    
+     return true;
+    }else{
+      
+      return false;
+    }
+      
   }
-  return false;
 }
 
 /**
@@ -29,8 +40,15 @@ function palindromeSwapper(str) {
  * @returns {boolean} true bila kata adalah palindrom
  */
 function isPalindrome(str) {
-  if (str.split('').reverse().join() === str) return console.log(true);
-  return console.log(false);
+  
+  if (str.split('').reverse().join('') === str) {
+    return true;
+  }else{
+
+    return false
+  }
+
+  
 }
 
 console.log(palindromeSwapper('arcecar')); // TRUE
