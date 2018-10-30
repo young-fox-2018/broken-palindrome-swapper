78521de0b27c7479d239671daf63b95a582497e2
fauzan_bintang <obin.aji@gmail.com>
2018-10-30 18:53:36 +0700

    broken-palindrome-swapper done

--- palindromeSwapper.js
+++ palindromeSwapper.js
@@ -6,16 +6,19 @@
  * @returns {boolean} true bila kata yang di-swap adalah palindrom
  */
 function palindromeSwapper(str) {
-  let newStr = '';
-  for (var i = 0; i < str.length; i++) {
-    for (var j = 0; j < str.length; i++) {
+  // debugger
+  if (isPalindrome(str)) return true;
+  for (var i = 0; i < str.length-1; i++) {
+    let newStr = '';
+    for (var j = 0; j < str.length; j++) {
       if (i === j) {
-        newStr += str[j++];
+        newStr += str[j+1];
         newStr += str[j];
         j++;
       } else {
         newStr += str[j];
       }
+      debugger
     }
     if (isPalindrome(newStr)) return true;
   }
@@ -29,8 +32,8 @@ function palindromeSwapper(str) {
  * @returns {boolean} true bila kata adalah palindrom
  */
 function isPalindrome(str) {
-  if (str.split('').reverse().join() === str) return console.log(true);
-  return console.log(false);
+  if (str.split('').reverse().join('') === str) return true;
+  return false;
 }
 
 console.log(palindromeSwapper('arcecar')); // TRUE
