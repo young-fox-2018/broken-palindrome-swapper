e12e911e48631b52c75332330d19ef298e8368e1
Desy Armariena <desy.armariena@gmail.com>
2018-10-30 17:28:54 +0700

    Debug for palindrome swapper

--- palindromeSwapper.js
+++ palindromeSwapper.js
@@ -6,20 +6,24 @@
  * @returns {boolean} true bila kata yang di-swap adalah palindrom
  */
 function palindromeSwapper(str) {
-  let newStr = '';
-  for (var i = 0; i < str.length; i++) {
-    for (var j = 0; j < str.length; i++) {
-      if (i === j) {
-        newStr += str[j++];
-        newStr += str[j];
-        j++;
-      } else {
-        newStr += str[j];
-      }
+  if ( isPalindrome(str)) return true;
+
+    for ( var i = 0; i < str.length; i++ ) {
+        let newStr = '';
+        for ( var j = 0; j < str.length; j++) {
+            // debugger
+            if ( i === j ) {
+                newStr += str[j+1];
+                newStr += str[j];
+                j++;
+            } else {
+                newStr += str[j];
+            }
+            // debugger
+        }
+        if ( isPalindrome(newStr)) return true;
     }
-    if (isPalindrome(newStr)) return true;
-  }
-  return false;
+    return false;
 }
 
 /**
@@ -29,8 +33,8 @@ function palindromeSwapper(str) {
  * @returns {boolean} true bila kata adalah palindrom
  */
 function isPalindrome(str) {
-  if (str.split('').reverse().join() === str) return console.log(true);
-  return console.log(false);
+  if (str.split('').reverse().join('') === str) return true;
+  return false;
 }
 
 console.log(palindromeSwapper('arcecar')); // TRUE
