fa7b24014c017bf168077e454fdccc69c09e8ef5
Atras Tudhi <atras.r@gmail.com>
2018-10-30 11:05:10 +0700

    broken palindrome swapper challange done

--- palindromeSwapper.js
+++ palindromeSwapper.js
@@ -7,19 +7,27 @@
  */
 function palindromeSwapper(str) {
   let newStr = '';
+
+  if(isPalindrome(str) === true) {
+    return true
+  }
+
   for (var i = 0; i < str.length; i++) {
-    for (var j = 0; j < str.length; i++) {
+    newStr = ''
+    for (var j = 0; j < str.length; j++) {
       if (i === j) {
-        newStr += str[j++];
+        newStr += str[j+1];
         newStr += str[j];
         j++;
       } else {
         newStr += str[j];
       }
     }
-    if (isPalindrome(newStr)) return true;
+    if (isPalindrome(newStr) === true) {
+      return true
+    } 
   }
-  return false;
+  return false
 }
 
 /**
@@ -29,10 +37,16 @@ function palindromeSwapper(str) {
  * @returns {boolean} true bila kata adalah palindrom
  */
 function isPalindrome(str) {
-  if (str.split('').reverse().join() === str) return console.log(true);
-  return console.log(false);
+  if (str.split('').reverse().join('') === str) {
+    return true 
+  } else {
+    return false
+  }
+  
 }
 
 console.log(palindromeSwapper('arcecar')); // TRUE
 console.log(palindromeSwapper('racecar')); // TRUE
 console.log(palindromeSwapper('recacar')); // FALSE
+
+console.log(palindromeSwapper('raeccar')); // TRUE
