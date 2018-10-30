35bbc57d1db76b2134e893b798a9c8e7241fa2b0
Arnold Herod <arnold.herod@gmail.com>
2018-10-30 10:36:03 +0700

    initial submission

--- palindromeSwapper.js
+++ palindromeSwapper.js
@@ -6,20 +6,26 @@
  * @returns {boolean} true bila kata yang di-swap adalah palindrom
  */
 function palindromeSwapper(str) {
-  let newStr = '';
-  for (var i = 0; i < str.length; i++) {
-    for (var j = 0; j < str.length; i++) {
-      if (i === j) {
-        newStr += str[j++];
-        newStr += str[j];
-        j++;
-      } else {
-        newStr += str[j];
-      }
-    }
-    if (isPalindrome(newStr)) return true;
+  if(isPalindrome(str)){
+    return true
   }
-  return false;
+  else{
+    let newStr = '';
+    for (var i = 0; i < str.length; i++) {
+      for (var j = 0; j < str.length; j++) {
+        debugger
+        if (i === j) {
+          j++;
+          newStr += str[j];
+          newStr += str[i];
+        } else {
+          newStr += str[j];
+        }
+      }
+      debugger
+      return isPalindrome(newStr);
+    }
+  }
 }
 
 /**
@@ -29,10 +35,14 @@ function palindromeSwapper(str) {
  * @returns {boolean} true bila kata adalah palindrom
  */
 function isPalindrome(str) {
-  if (str.split('').reverse().join() === str) return console.log(true);
-  return console.log(false);
+  if (str.split('').reverse().join("") === str) {
+    return true;
+  }
+  else{
+    return false;
+  }
 }
 
 console.log(palindromeSwapper('arcecar')); // TRUE
-console.log(palindromeSwapper('racecar')); // TRUE
+console.log(palindromeSwapper('racecar')); // FALSE
 console.log(palindromeSwapper('recacar')); // FALSE
