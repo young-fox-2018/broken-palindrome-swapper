987cddc4909bb114752fda8bd53b189dff2e42e9
Mochamad Faishal Amir <faishalkings@gmail.com>
2018-10-30 16:00:19 +0700

    commit 1

--- palindromeSwapper.js
+++ palindromeSwapper.js
@@ -6,20 +6,26 @@
  * @returns {boolean} true bila kata yang di-swap adalah palindrom
  */
 function palindromeSwapper(str) {
-  let newStr = '';
-  for (var i = 0; i < str.length; i++) {
-    for (var j = 0; j < str.length; i++) {
+  for (var i = 0; i < str.length-1; i++) {
+    let newStr = '';
+    for (var j = i; j < str.length; j++) {
       if (i === j) {
-        newStr += str[j++];
+        newStr += str[j+1];
         newStr += str[j];
         j++;
       } else {
         newStr += str[j];
       }
     }
-    if (isPalindrome(newStr)) return true;
+    if (isPalindrome(newStr) || isPalindrome(str)) {
+      return true;
+    }
+    else {
+      return false;
+    }
   }
-  return false;
+
+
 }
 
 /**
@@ -29,8 +35,12 @@ function palindromeSwapper(str) {
  * @returns {boolean} true bila kata adalah palindrom
  */
 function isPalindrome(str) {
-  if (str.split('').reverse().join() === str) return console.log(true);
-  return console.log(false);
+  if (str.split('').reverse().join('') === str) {
+    return true;
+  }
+  else {
+    return false;
+  }
 }
 
 console.log(palindromeSwapper('arcecar')); // TRUE
