0f91d67cc9f8fdb627b97359af7e29b043c82a98
Januarsmad <januarsmad@gmail.com>
2018-10-30 18:11:02 +0700

    program done

--- palindromeSwapper.js
+++ palindromeSwapper.js
@@ -6,20 +6,22 @@
  * @returns {boolean} true bila kata yang di-swap adalah palindrom
  */
 function palindromeSwapper(str) {
-  let newStr = '';
-  for (var i = 0; i < str.length; i++) {
-    for (var j = 0; j < str.length; i++) {
-      if (i === j) {
-        newStr += str[j++];
-        newStr += str[j];
-        j++;
-      } else {
-        newStr += str[j];
+  if(isPalindrome(str)) return true
+  for(var i = 0; i < str.length; i++){
+      let newStr = ''
+      for(var j = 0; j < str.length; j++){
+          if(i === j){
+              newStr += str[j+1]
+              newStr += str[j]
+              j++ 
+          }else{
+              newStr += str[j]
+          }
+          debugger
       }
-    }
-    if (isPalindrome(newStr)) return true;
+      if(isPalindrome(newStr)) return true
   }
-  return false;
+  return false
 }
 
 /**
@@ -28,9 +30,10 @@ function palindromeSwapper(str) {
  * @param {string} str - input kata yang dicek bila palindrom
  * @returns {boolean} true bila kata adalah palindrom
  */
-function isPalindrome(str) {
-  if (str.split('').reverse().join() === str) return console.log(true);
-  return console.log(false);
+
+function isPalindrome(str){
+  if (str.split('').reverse().join('') === str) return true
+  return false
 }
 
 console.log(palindromeSwapper('arcecar')); // TRUE
