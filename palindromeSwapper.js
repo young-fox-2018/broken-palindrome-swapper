c7362a829957d094ed8412c608cd1d97f760fe7a
Cascade <mufhabu@gmail.com>
2018-10-30 14:21:01 +0700

    done palindrome

--- palindromeSwapper.js
+++ palindromeSwapper.js
@@ -4,13 +4,14 @@
  * contoh: makan => amkan, mkaan, maakn, makna
  * @param {string} str - input kata yang ingin di-swap dan dicek palindrom
  * @returns {boolean} true bila kata yang di-swap adalah palindrom
- */
+//  */
 function palindromeSwapper(str) {
-  let newStr = '';
+  if (isPalindrome(str)) return true;
   for (var i = 0; i < str.length; i++) {
-    for (var j = 0; j < str.length; i++) {
+    let newStr = '';
+    for (var j = 0; j < str.length; j++) {
       if (i === j) {
-        newStr += str[j++];
+        newStr += str[j + 1];
         newStr += str[j];
         j++;
       } else {
@@ -22,17 +23,19 @@ function palindromeSwapper(str) {
   return false;
 }
 
-/**
+/*
  * @function isPalindrome
  * Cek apabila kata merupakan palindromeSwapper
  * @param {string} str - input kata yang dicek bila palindrom
  * @returns {boolean} true bila kata adalah palindrom
  */
+
 function isPalindrome(str) {
-  if (str.split('').reverse().join() === str) return console.log(true);
-  return console.log(false);
+  if (str.split('').reverse().join("") === str) return true;
+  return false; 
 }
 
 console.log(palindromeSwapper('arcecar')); // TRUE
 console.log(palindromeSwapper('racecar')); // TRUE
 console.log(palindromeSwapper('recacar')); // FALSE
+
