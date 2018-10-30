d608ea9d4f2c5962033831c684030de6db9ab8ef
Kevin Wijaya <Kevin@Macbooknyaa.localdomain>
2018-10-30 10:39:08 +0700

    submitting this ex

--- palindromeSwapper.js
+++ palindromeSwapper.js
@@ -6,18 +6,26 @@
  * @returns {boolean} true bila kata yang di-swap adalah palindrom
  */
 function palindromeSwapper(str) {
-  let newStr = '';
-  for (var i = 0; i < str.length; i++) {
-    for (var j = 0; j < str.length; i++) {
-      if (i === j) {
-        newStr += str[j++];
+  if(isPalindrome(str)){
+    return true;
+    brak
+  }
+  for (var i = 0; i < str.length-1; i++) {
+    let newStr = '';
+    for (var j = 0; j < str.length; j++) {
+      if (i === j ) {
+        newStr += str[j+1];
         newStr += str[j];
         j++;
       } else {
         newStr += str[j];
       }
+      debugger
     }
-    if (isPalindrome(newStr)) return true;
+    if (isPalindrome(newStr)){
+      return true
+      break;
+    }
   }
   return false;
 }
@@ -29,8 +37,11 @@ function palindromeSwapper(str) {
  * @returns {boolean} true bila kata adalah palindrom
  */
 function isPalindrome(str) {
-  if (str.split('').reverse().join() === str) return console.log(true);
-  return console.log(false);
+  if (str.split('').reverse().join("") === str){
+    return true
+  }else{
+    return false
+  }
 }
 
 console.log(palindromeSwapper('arcecar')); // TRUE
