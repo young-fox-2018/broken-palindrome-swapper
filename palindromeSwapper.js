4b2690d940005e1cf47d75d10428725afe095322
sendyakbar <msendyakbars@gmail.com>
2018-10-30 10:22:49 +0700

    debugging broken palindrome swapper done

--- palindromeSwapper.js
+++ palindromeSwapper.js
@@ -6,18 +6,22 @@
  * @returns {boolean} true bila kata yang di-swap adalah palindrom
  */
 function palindromeSwapper(str) {
-  let newStr = '';
-  for (var i = 0; i < str.length; i++) {
-    for (var j = 0; j < str.length; i++) {
+  for (var i = 0; i < str.length - 1; i++) {
+    let newStr = '';
+    debugger
+    for (var j = i; j < str.length; j++) {
+      debugger
       if (i === j) {
-        newStr += str[j++];
+        newStr += str[j + 1];
         newStr += str[j];
+        debugger
         j++;
       } else {
         newStr += str[j];
+        debugger
       }
     }
-    if (isPalindrome(newStr)) return true;
+    if (isPalindrome(newStr) || isPalindrome(str)) return true;
   }
   return false;
 }
@@ -29,8 +33,8 @@ function palindromeSwapper(str) {
  * @returns {boolean} true bila kata adalah palindrom
  */
 function isPalindrome(str) {
-  if (str.split('').reverse().join() === str) return console.log(true);
-  return console.log(false);
+  if (str.split('').reverse().join('') === str) return true;
+  return false;
 }
 
 console.log(palindromeSwapper('arcecar')); // TRUE
